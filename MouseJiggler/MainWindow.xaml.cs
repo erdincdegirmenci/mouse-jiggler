c7d9e752bea17c935a603fcc4de8f525f4aecf0e
Erdinc.Degirmenci <Erdinc.Degirmenci@ERDINCDEGIRMENC>
2025-08-20 10:52:35 +0300

    first commit

---
+++ MouseJiggler/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+
+namespace MouseJiggler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private MouseJigglers jiggler;
+        private NotifyIcon  trayIcon;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            jiggler = new MouseJigglers();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(IntervalBox.Text, out int interval))
+            {
+                if (interval < 5000)
+                {
+                    System.Windows.MessageBox.Show("Minimum interval is 5000 ms. Setting to 5000.");
+                    interval = 5000;
+                    IntervalBox.Text = interval.ToString();
+                }
+
+                jiggler = new MouseJigglers(interval);
+                jiggler.Start();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Please enter a valid number.");
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e) => jiggler.Stop();
+
+        private void EnableTray_Click(object sender, RoutedEventArgs e)
+        {
+            if (trayIcon == null)
+            {
+                // Tray icon oluştur
+                trayIcon = new NotifyIcon();
+                trayIcon.Icon = System.Drawing.SystemIcons.Application;
+
+                trayIcon.Visible = true;
+                trayIcon.Text = "Mouse Jiggler";
+
+                // Çift tıklama ile pencereyi geri getir
+                trayIcon.DoubleClick += (s, args) =>
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                    this.ShowInTaskbar = true;
+
+                    // Tray icon'u kaldır
+                    trayIcon.Visible = false;
+                    trayIcon.Dispose();
+                    trayIcon = null;
+                };
+
+                // Pencereyi gizle
+                this.Hide();
+            }
+            else
+            {
+                // Pencereyi göster
+                this.Show();
+                this.WindowState = WindowState.Normal;
+                this.ShowInTaskbar = true;
+
+                // Tray icon'u kaldır
+                trayIcon.Visible = false;
+                trayIcon.Dispose();
+                trayIcon = null;
+            }
+        }
+
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            base.OnClosing(e);
+        }
+    }
+}
---
+++ MouseJiggler/MouseJigglers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseJiggler
+{
+    public class MouseJigglers
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        public struct POINT { public int X; public int Y; }
+
+        private Thread jiggleThread;
+        private bool running = false;
+        private int interval;
+
+        public MouseJigglers(int intervalMs = 5000)
+        {
+            interval = intervalMs;
+        }
+
+        public void Start()
+        {
+            if (running) return;
+            running = true;
+            jiggleThread = new Thread(JiggleLoop) { IsBackground = true };
+            jiggleThread.Start();
+        }
+
+        public void Stop()
+        {
+            running = false;
+        }
+
+        private void JiggleLoop()
+        {
+            while (running)
+            {
+                GetCursorPos(out POINT p);
+                SetCursorPos(p.X + 1, p.Y);
+                Thread.Sleep(interval);
+                SetCursorPos(p.X, p.Y);
+                Thread.Sleep(interval);
+            }
+        }
+    }
+}
